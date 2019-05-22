e0139c874d0f7afa163311f8e09866c258abb026
Anton <antbabichev@gmail.com>
2019-05-22 23:43:54 +0700

    update kanguruShow

--- src/KangarooShow.java
+++ src/KangarooShow.java
@@ -1,21 +1,38 @@
+import java.util.Scanner;
+
 public class KangarooShow {
 
     public static void main (String args[]){
-        isKangarooCrossing(2, 1, 1, 2);
+        boolean result;
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите позицию первого кенгуру x1: ");
+        int x1 = scanner.nextInt();
+        System.out.print("Введите длину прыжка первого кенгуру v1: ");
+        int v1 = scanner.nextInt();
+        System.out.print("Введите позицию второго кенгуру x2: ");
+        int x2 = scanner.nextInt();
+        System.out.print("Введите длину прыжка второго кенгуру v2: ");
+        int v2 = scanner.nextInt();
+
+        result = isKangarooCrossing(x1, v1, x2, v2);
+        System.out.println(result);
     }
 
     public static boolean isKangarooCrossing(int x1, int v1, int x2, int v2) {
 
-        int king1 = x1 + v1;
-        int king2 = x2 + v2;
+//      return (x1 + v1 == x2 + v2) ? true :  false;
 
-        if (king1 == king2) {
-            System.out.println(true);
-        } else {
-            System.out.println(false);
-        }
+        return (x1 + v1 == x2 + v2);
 
-        return false;
+
+
+//        int king1 = x1 + v1;
+//        int king2 = x2 + v2;
+//
+//        if (king1 == king2) {
+//            return true;
+//        }
+//        return false;
     }
 }
